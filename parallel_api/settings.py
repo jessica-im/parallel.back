8cd556cb6f97982b39653599adb23ef2ef0b428f
Jessica Im <me@jessicaim.com>
2021-12-01 15:04:53 -0500

    updated profile model to reflect one to one relationship with useraccount model

--- parallel_api/settings.py
+++ parallel_api/settings.py
@@ -30,7 +30,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY='django-insecure-p6o&48ulfodajvpw5vv+6da&_b)ocwgd7(wh($d@!_0d040tj*'
+# SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- users_api/models.py
+++ users_api/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class UserAccount(models.Model):
+    username = models.CharField(max_length=75, unique=True)
+    password = models.CharField(max_length=1000)
+    # profile = models.ForeignKey(Profile, default=0, on_delete=models.CASCADE)
+
 class Profile(models.Model):
+    user = models.OneToOneField(UserAccount, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=75)
     image = models.CharField(max_length=1000)
     status = models.CharField(max_length=100)
     website = models.CharField(max_length=1000)
 
-class UserAccount(models.Model):
-    username = models.CharField(max_length=75, unique=True)
-    password = models.CharField(max_length=1000)
-    profile = models.ForeignKey(Profile, default=0, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.user.username} Profile'
 
 class Project(models.Model):
     profile = models.ForeignKey(Profile, default=0, on_delete=models.CASCADE)

9a84437942ce70ed718b4017386bd8692a825431
Jessica Im <me@jessicaim.com>
2021-12-05 14:21:05 -0500

    updated check login within views

--- users_api/views.py
+++ users_api/views.py
@@ -42,12 +42,12 @@ def check_login(request):
     if request.method=='PUT':
 
         jsonRequest = json.loads(request.body) #make the request JSON format
-        email = jsonRequest['email'] #get the email from the request
+        username = jsonRequest['username'] #get the email from the request
         password = jsonRequest['password'] #get the password from the request
-        if UserAccount.objects.get(email=email): #see if email exists in db
-            user = UserAccount.objects.get(email=email)  #find user object with matching email
+        if UserAccount.objects.get(username=username): #see if email exists in db
+            user = UserAccount.objects.get(username=username)  #find user object with matching email
             if check_password(password, user.password): #check if passwords match
-                return JsonResponse({'id': user.id, 'email': user.email}) #if passwords match, return a user dict
+                return JsonResponse({'id': user.id, 'username': user.username}) #if passwords match, return a user dict
             else: #passwords don't match so return empty dict
                 return JsonResponse({})
         else: #if email doesn't exist in db, return empty dict
